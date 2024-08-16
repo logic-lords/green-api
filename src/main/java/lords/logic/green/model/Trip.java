376add327231e6ba8055da91a3dcb845db8a181b
Anjaraniaina <hei.anjaraniaina@gmail.com>
2024-08-16 21:57:51 +0300

    feat: db setup

--- src/main/java/lords/logic/green/model/Trip.java
+++ src/main/java/lords/logic/green/model/Trip.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lords.logic.green.model.enums.TransportType;
@@ -21,15 +22,19 @@ public class Trip {
     private Double distance;
 
     @ManyToOne
+    @JoinColumn(name ="start_location_id")
     private Location startLocation;
 
     @ManyToOne
+    @JoinColumn(name ="end_location_id")
     private Location endLocation;
 
     @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne
+    @JoinColumn(name = "transport_id")
     private Transport transport;
 
     @CreationTimestamp
--- src/main/java/lords/logic/green/model/User.java
+++ src/main/java/lords/logic/green/model/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,12 +15,14 @@ import java.time.Instant;
 
 @Entity
 @Builder
+@Table(name = "\"user\"")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
     private String email;
     private String phoneNumber;
     private String firstName;
